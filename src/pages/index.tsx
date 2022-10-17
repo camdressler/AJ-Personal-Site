47927094d4aecf5bc6f8356291b0ae752631847e
AJ Matise <105606366+AJ-Matise@users.noreply.github.com>
2022-10-17 11:35:13 -0700

    Update index.tsx
    Fixed </p> bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,7 +38,7 @@ export default function Home() {
       </a>
       <div className="w-[75%] mx-auto flex flex-col items-center justify-center text-center pt-25 space-x-2 break-words text-white">
         
-        <p><br></br>I'm a 17 year old entrepreneur helping people make hundreds of thousands of dollars reselling sneakers! <br></br><br></br><p>My goal in life is to provide value to my customers, elevate my staff teams, and build positive communities in the reselling scene. </p>
+        <p></p><br></br>I'm a 17 year old entrepreneur helping people make hundreds of thousands of dollars reselling sneakers! <br></br><br></br><p>My goal in life is to provide value to my customers, elevate my staff teams, and build positive communities in the reselling scene. </p>
       </div>
     </div>
   );
